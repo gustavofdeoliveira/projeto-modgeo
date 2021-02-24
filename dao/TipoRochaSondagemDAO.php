64c1d7ab23a7c0ff11a6401937002a47235af577
Gustavo Ferreira <65983794+gustavoofdeooliveira@users.noreply.github.com>
2021-02-24 17:14:32 -0300

    TipoRocha feita

--- dao/TipoRochaSondagemDAO.php
+++ dao/TipoRochaSondagemDAO.php
@@ -1,7 +1,20 @@
 <?php
+session_start();
+require_once ("../control/Conexao.php");
 
+class TipoRochaSondagemDAO{
+    private $conn;
+   function __construct()
+   {
+        $this->conn=Conexao::conectar();
+   }
+   function inserir(TipoRochaSondagem $modelo){
+       $sql = "insert into tipo_rocha_sondagem(profundidade_min, profundidade_max, tipo rocha, id_nome_sondagem)values('".$modelo->getProfMin()."','".$modelo->getProfMax()."','".$modelo->getTipoRocha()."','".
+       $modelo->getIdNomeSondagem()."')";
 
-class TipoRochaSondagemDAO
-{
+       
+       $this->conn->exec($sql);
+      $_SESSION["id"]=$this->conn->lastInsertId();
 
+   }
 }
