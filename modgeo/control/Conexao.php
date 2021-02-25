585fcf97c899422edfb92bbcedec36340ac30a97
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-25 11:51:54 -0300

    alterando conexao

--- modgeo/control/Conexao.php
+++ modgeo/control/Conexao.php
@@ -1,10 +1,9 @@
-<?php
 class Conexao
 {
     static function conectar()
     {
         try {
-           $pdo = new PDO('mysql:host=localhost;dbname=modgeo', 'root', '');
+           $pdo = new PDO('mysql:host=localhost;dbname=modgeo', 'root', 'root');
           $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           return $pdo;
         } catch (PDOException $e) {
